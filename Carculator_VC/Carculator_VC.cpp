bf0c2dc705e8a271aa6578b2d043e706a71a6922
Андрей Курье <andrey.kurje@gmail.com>
2020-02-11 16:01:37 +0300

    Добавьте файлы проекта.

---
+++ Carculator_VC/Carculator_VC.cpp
@@ -0,0 +1,54 @@
+﻿// Carculator_VC.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "../../StaticLib_VC/StaticLib_VC/StaticLib_VC.h"
+using namespace std;
+
+
+int main()
+{
+	cout << "++ Calculator ++\n\n";
+
+
+	while (true)
+	{
+		cout << "Insert first number> ";
+		double a;
+		cin >> a;
+
+		cout << "Insert operation> ";
+		char operation;
+		cin >> operation;
+
+		cout << "Insert second number> ";
+		double b;
+		cin >> b;
+
+		double res;
+		bool error = false;
+
+		switch (operation)
+		{
+		case '+':
+			res = sl_VC_fPlus(a, b);
+			break;
+		case '-':
+			res = sl_VC_fMinus(a, b);
+			break;
+		case '*':
+			res = sl_VC_fMult(a, b);
+			break;
+		case '/':
+			res = sl_VC_fDiv(a, b);
+			break;
+		default:
+			error = true;
+			break;
+		}
+		if (error) { cout << "error"; }
+		else { cout << "result = " << res; }
+		cout << endl << endl;
+	}
+	system("pause");
+}
