182fcf6c66bdde50ab10e0a0850b890cda22394c
Андрей Курье <andrey.kurje@gmail.com>
2020-02-11 16:05:53 +0300

    Добавьте файлы проекта.

---
+++ StaticLib_VC/StaticLib_VC.cpp
@@ -0,0 +1,22 @@
+﻿// StaticLib_VC.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "StaticLib_VC.h"
+
+double sl_VC_fPlus(double a, double b) {
+	return a + b;
+}
+
+double sl_VC_fMult(double a, double b) {
+	return a * b;
+}
+
+double sl_VC_fDiv(double a, double b) {
+	return a / b;
+}
+
+double sl_VC_fMinus(double a, double b) {
+	return a - b;
+}
---
+++ StaticLib_VC/StaticLib_VC.h
@@ -0,0 +1,6 @@
+#pragma once
+
+double sl_VC_fPlus(double, double);
+double sl_VC_fMult(double, double);
+double sl_VC_fDiv(double, double);
+double sl_VC_fMinus(double, double);
