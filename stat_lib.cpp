b6c775ac0cc3454be7916ffe18e3223823392572
Deep-North <38556622+Deep-North@users.noreply.github.com>
2020-02-11 15:59:40 +0300

    static lib

---
+++ stat_lib.cpp
@@ -0,0 +1,17 @@
+#include "stat_lib.h"
+
+double sl_fPlus(double a, double b) {
+    return a + b;
+}
+
+double sl_fMult(double a, double b) {
+    return a * b;
+}
+
+double sl_fDiv(double a, double b) {
+    return a / b;
+}
+
+double sl_fMinus(double a, double b) {
+    return a - b;
+}
---
+++ stat_lib.h
@@ -0,0 +1,10 @@
+#ifndef STAT_LIB_H
+#define STAT_LIB_H
+
+
+double sl_fPlus(double, double);
+double sl_fMult(double, double);
+double sl_fDiv(double, double);
+double sl_fMinus(double, double);
+
+#endif // STAT_LIB_H
